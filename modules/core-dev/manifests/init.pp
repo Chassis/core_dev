c5a1b1e6778a2bc786e21577ccfb6b55acd4773a
K. Adam White <kadamwhite@users.noreply.github.com>
2019-03-06 14:29:57 -0500

    Simplify comment at head of init.pp

--- modules/core-dev/manifests/init.pp
+++ modules/core-dev/manifests/init.pp
@@ -1,22 +1,4 @@
-# Example Puppet class!
-#
-# Chassis automatically loads your class, and passes $config in, which is an
-# array representing the YAML-based configuration. You can use this to access
-# Chassis configuration, or your own custom keys. In this demo, we've used
-# `show_example`. To test it out, add the following to your config:
-#
-#     show_example: hello!
-#
-# The entirety of your behaviour should be wrapped inside this class.
-#
-# ***********************************
-#          IMPORTANT NOTES:
-#
-# * Your module directory must be named the same as the extension.
-# * Your class must be in init.pp, and named the same as the extension.
-#
-# ***********************************
-
+# Class to prepare the Chassis box for WP core development.
 class core-dev (
 	$config
 ) {
