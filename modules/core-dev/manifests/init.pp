4b7265dd285c67f7f5e258d8a5cb1ea8e1f9bded
K. Adam White <kadamwhite@users.noreply.github.com>
2019-03-06 14:28:02 -0500

    Temporarily disable site installation command
    This can be added back in once other dependencies are in place

--- modules/core-dev/manifests/init.pp
+++ modules/core-dev/manifests/init.pp
@@ -20,33 +20,33 @@
 class core-dev (
 	$config
 ) {
-	# Create vagrant-core.local
-	chassis::site { 'vagrant-core.local':
-		location          => '/vagrant/wordpress-develop/src',
-		wpdir             => '/vagrant/wordpress-develop/src',
-		contentdir        => '/vagrant/wordpress-develop/src/wp-content',
-		hosts             => ['vagrant-core.local'],
-		database          => 'vagrantcore_local',
-		database_user     => $config[database][user],
-		database_password => $config[database][password],
-		admin_user        => $config[admin][user],
-		admin_email       => $config[admin][email],
-		admin_password    => $config[admin][password],
-		sitename          => $config[site][name],
-		require => [
-			Class['chassis::php'],
-			Package['git-core'],
-			Class['mysql::server'],
-		]
-	}
+	# # Create vagrant-core.local
+	# chassis::site { 'vagrant-core.local':
+	# 	location          => '/vagrant/wordpress-develop/src',
+	# 	wpdir             => '/vagrant/wordpress-develop/src',
+	# 	contentdir        => '/vagrant/wordpress-develop/src/wp-content',
+	# 	hosts             => ['vagrant-core.local'],
+	# 	database          => 'vagrantcore_local',
+	# 	database_user     => $config[database][user],
+	# 	database_password => $config[database][password],
+	# 	admin_user        => $config[admin][user],
+	# 	admin_email       => $config[admin][email],
+	# 	admin_password    => $config[admin][password],
+	# 	sitename          => $config[site][name],
+	# 	require => [
+	# 		Class['chassis::php'],
+	# 		Package['git-core'],
+	# 		Class['mysql::server'],
+	# 	]
+	# }
 
-# 	package { 'php-package-name':
-# 		ensure  => $package
-# 	}
-#
-# 	file { '/tmp/randomfile.ini':
-# 		ensure => $file,
-# 		content => '# Example content',
-# 		force  => true
-# 	}
+	# package { 'php-package-name':
+	# 	ensure  => $package
+	# }
+
+	# file { '/tmp/randomfile.ini':
+	# 	ensure => $file,
+	# 	content => '# Example content',
+	# 	force  => true
+	# }
 }
