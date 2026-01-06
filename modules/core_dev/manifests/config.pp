724f9fe498464114ea7b267dc859eb933c67ad22
Bronson Quick <bronson.quick@akcelo.com>
2026-01-06 16:39:10 +1000

    Update for Noble Numbat

--- modules/core_dev/manifests/config.pp
+++ modules/core_dev/manifests/config.pp
@@ -1,17 +1,17 @@
 # Adjust the default Chassis wp-config.php
 class core_dev::config {
-	# Configure Chassis' wp-config.php per the WP Core Development guide:
-	# http://docs.chassis.io/en/latest/guides/#wordpress-core-development
-	file_line { 'adjust chassis wp-config.php':
-		path     => '/vagrant/wp-config.php',
-		multiple => true,
-		line     => "if ( ! defined( 'WP_CLI' ) ) { \n\trequire_once( ABSPATH . 'wp-settings.php' );\n}",
-		match    => 'require_once.*ABSPATH.*wp-settings.php',
-	}
+  # Configure Chassis' wp-config.php per the WP Core Development guide:
+  # http://docs.chassis.io/en/latest/guides/#wordpress-core-development
+  file_line { 'adjust chassis wp-config.php':
+    path     => '/vagrant/wp-config.php',
+    multiple => true,
+    line     => "if ( ! defined( 'WP_CLI' ) ) { \n\trequire_once( ABSPATH . 'wp-settings.php' );\n}",
+    match    => 'require_once.*ABSPATH.*wp-settings.php',
+  }
 
-	# Add the proxy wp-config.php file to the `src/` folder.
-	file { '/vagrant/wordpress-develop/src/wp-config.php':
-		content => template('core_dev/wp-config.php.erb'),
-		require => Class['core_dev::repository'],
-	}
+  # Add the proxy wp-config.php file to the `src/` folder.
+  file { '/vagrant/wordpress-develop/src/wp-config.php':
+    content => template('core_dev/wp-config.php.erb'),
+    require => Class['core_dev::repository'],
+  }
 }
--- modules/core_dev/manifests/init.pp
+++ modules/core_dev/manifests/init.pp
@@ -1,25 +1,26 @@
 # Class to prepare the Chassis box for WP core development.
+#
+# @param config Configuration hash containing database and other settings
 class core_dev (
-	$config
+  Hash $config
 ) {
-	class { 'core_dev::repository':
-		config => $config,
-	}
+  class { 'core_dev::repository':
+    config => $config,
+  }
 
-	class { 'core_dev::config':
-		require => Class['core_dev::repository'],
-	}
+  class { 'core_dev::config':
+    require => Class['core_dev::repository'],
+  }
+  class { 'core_dev::tests':
+    database          => "${ config[database][name] }_tests",
+    database_user     => $config[database][user],
+    database_password => $config[database][password],
+    database_host     => 'localhost',
+    database_prefix   => 'wptests_',
+    require           => Class['core_dev::repository'],
+  }
 
-	class { 'core_dev::tests':
-		database          => "${ config[database][name] }_tests",
-		database_user     => $config[database][user],
-		database_password => $config[database][password],
-		database_host     => 'localhost',
-		database_prefix   => 'wptests_',
-		require           => Class['core_dev::repository'],
-	}
-
-	class { 'core_dev::build':
-		require => Class['core_dev::config'],
-	}
+  class { 'core_dev::build':
+    require => Class['core_dev::config'],
+  }
 }
